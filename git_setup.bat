6536132af3b52dfc4dcd7892f47f033dde817231
Rance Cheng <rancecheng@nics.nat.gov.tw>
2026-04-21 16:47:00 +0800

    refactor: Financial_Accounting Ver_0.1

--- git_setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d D:\WorkspaceCopilot\Financial_Accounting
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/RanceCheng/Financial_Accounting.git
-git branch -M main
-echo.
-echo === Done ===
-echo Now run: git push -u origin main
